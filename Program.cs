d7a66875c3700dd0a86d1e2f19a28e3e4ff2193c
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-12-13 11:59:18 +0900

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Hello!");
+
+foreach(var type in typeof(X86Base).Assembly.GetTypes().Where(x => (typeof(X86Base).IsAssignableFrom(x) || typeof(ArmBase).IsAssignableFrom(x)) && x.GetProperty("IsSupported") is not null))
+{
+    var prop = type.GetProperty("IsSupported")!;
+    Console.WriteLine($"{type.Namespace![(type.Namespace!.LastIndexOf(".") + 1)..]} {type.Name} => {(((bool)prop.GetValue(null)!) ? "Support" : "Not Support")}");
+}
+
+return;
